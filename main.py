450d33157f5f1e16119058d1a9b70d89a35f231d
keei051 <keei@inbox.ru>
2025-07-09 06:14:47 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
 from config import BOT_TOKEN, VK_TOKEN
 from handlers import setup_handlers
 from database import init_db
@@ -9,14 +10,22 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 async def main():
-    bot = Bot(token=BOT_TOKEN)
-    await bot.delete_webhook(drop_pending_updates=True)
+    bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
     dp = Dispatcher()
+
+    # Подключение базы данных
     await init_db()
+
+    # Регистрация хендлеров
     setup_handlers(dp)
+
     logger.info("Бот запущен!")
+
+    # Запуск бота
     try:
         await dp.start_polling(bot)
+    except (KeyboardInterrupt, SystemExit):
+        logger.info("Бот остановлен вручную.")
     except Exception as e:
         logger.error(f"Ошибка при запуске: {e}")
 
