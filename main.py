a8769ab9237967107a9459ed60547ead2dd33965
keei051 <keei@inbox.ru>
2025-07-09 05:54:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,23 +5,15 @@ from config import BOT_TOKEN, VK_TOKEN
 from handlers import setup_handlers
 from database import init_db
 
-# Настройка логирования
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 async def main():
-    # Инициализация бота и диспетчера
     bot = Bot(token=BOT_TOKEN)
-    await bot.delete_webhook(drop_pending_updates=True)  # Удаляем вебхук для избежания конфликтов
+    await bot.delete_webhook(drop_pending_updates=True)
     dp = Dispatcher()
-    
-    # Инициализация базы данных
     await init_db()
-    
-    # Настройка обработчиков
     setup_handlers(dp)
-    
-    # Запуск бота
     logger.info("Бот запущен!")
     try:
         await dp.start_polling(bot)
