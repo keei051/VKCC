b004d8b54ddd5aca64d3646f0a9761ad26c69d24
keei051 <keei@inbox.ru>
2025-07-11 20:38:05 +0500

    Update router.py

--- router.py
+++ router.py
@@ -1,6 +1,7 @@
 from aiogram import Dispatcher
 from handlers import handlers, callback_handlers
 
-def setup_routers(dp: Dispatcher):
+def setup_handlers(dp: Dispatcher):
     dp.include_router(handlers.router)
     dp.include_router(callback_handlers.router)
+
