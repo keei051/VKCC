9c0413be6d55794465abbeedb687d7b895c85750
keei051 <keei@inbox.ru>
2025-07-11 20:38:59 +0500

    Update session.py

--- session.py
+++ session.py
@@ -1,13 +1,18 @@
 import aiohttp
+import logging
 
+logger = logging.getLogger(__name__)
 session: aiohttp.ClientSession = None
 
 async def create_session():
     global session
-    session = aiohttp.ClientSession()
+    if session is None or session.closed:
+        session = aiohttp.ClientSession()
+        logger.info("HTTP-сессия создана.")
 
 async def close_session():
     global session
-    if session:
+    if session and not session.closed:
         await session.close()
+        logger.info("HTTP-сессия закрыта.")
         session = None
