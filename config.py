1be4647b9b4528ad8926cd75027feb3086b24bed
keei051 <keei@inbox.ru>
2025-07-12 10:27:06 +0500

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 VK_TOKEN = os.getenv("VK_TOKEN")
-
-if not BOT_TOKEN or not VK_TOKEN:
-    raise ValueError("Необходимо указать BOT_TOKEN и VK_TOKEN в переменных окружения.")
-
 MAX_LINKS_PER_BATCH = 50
+DATABASE_URL = os.getenv("DATABASE_URL")  # postgresql://user:pass@host/db
+
+if not BOT_TOKEN or not VK_TOKEN or not DATABASE_URL:
+    raise EnvironmentError("Необходимо указать BOT_TOKEN, VK_TOKEN и DATABASE_URL в переменных окружения.")
